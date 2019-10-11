4f63721822093812618f8c3f8a751d58ac047939
Nwakwuo Uche <dukweb4@gmail.com>
2019-10-12 00:18:12 +0100

    moved our directory and shop datainto its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

---
+++ src/redux/shop/shop.selector.js
@@ -0,0 +1,24 @@
+import { createSelector } from 'reselect';
+
+const selectShop = state => state.shop;
+
+export const selectCollections = createSelector(
+  [selectShop],
+  shop => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = collectionUrlParam => 
+  createSelector(
+    [selectCollections],
+    
+    //if data was an array of objects
+    // collections => collections.find(collecton => collecton.id === COLLECTION_ID_MAP[collectionUrlParam]
+    
+    collections => collections[collectionUrlParam]
+  )
+
